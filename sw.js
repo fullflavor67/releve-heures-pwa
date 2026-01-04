d07cbd9cb90b86a34210a77ff257e6c21396ded0
fullflavor67 <christiangrasser67@gmail.com>
2026-01-04 16:02:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,17 +8,30 @@ const urlsToCache = [
   "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+// Installation du service worker
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+// Activation
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+// Interception des requêtes
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
